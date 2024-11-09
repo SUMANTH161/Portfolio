26af5733d45e46735db9cb1ba60ebbf83839db3b
SUMANTH <93914950+SUMANTH161@users.noreply.github.com>
2024-11-10 02:05:07 +0530

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,4 +1,6 @@
 <?php
+error_log("Request method: " . $_SERVER['REQUEST_METHOD']);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
